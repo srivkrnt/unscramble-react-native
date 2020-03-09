26745759c081215abaef026371171d1c7ea1f37f
srivkrnt <sri.vkrnt@gmail.com>
2020-03-09 12:29:24 +0530

    replace icons and fix question box issue

--- components/Question.js
+++ components/Question.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor:'#F48023',
         margin:2,
-        height:'10%'
+        height:45
     },
     questionContainer : {
         alignSelf : "center",
